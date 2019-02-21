0a66a233633022e735572311a760eed277376fe2
Defaultplayer001 <Defaultplayer001@gmail.com>
2019-02-20 23:37:28 -0700

    CU Setup name updates

--- [1.0] Deus Ex - Windows-Linux-macOS-Android/CommunityUpdateFileArchiveDXPC/SystemFiles.exe Creators/CUSetupCreator.bat
+++ [1.0] Deus Ex - Windows-Linux-macOS-Android/CommunityUpdateFileArchiveDXPC/SystemFiles.exe Creators/CUSetupCreator.bat
@@ -1,1 +1,1 @@
-copy /b 7zS.sfx + ConfigCUSetupFiles.txt + "Deus Ex 1 Community Update - Deus Ex - PC.7z" "Deus Ex 1 Community Update.exe"
+copy /b 7zS.sfx + ConfigCUSetupFiles.txt + "[1.0] Deus Ex - Windows-Linux-macOS-Android.7z" "Deus Ex 1 Community Update.exe"
